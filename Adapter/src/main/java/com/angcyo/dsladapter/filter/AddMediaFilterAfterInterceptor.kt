459a16a3936de25051e61066f097a033c00b2b35
angcyo <angcyo@126.com>
2020-03-28 23:46:35 +0800

    + 控制`RecycleView`最大显示数量的`FilterAfterInterceptor`

---
+++ Adapter/src/main/java/com/angcyo/dsladapter/filter/AddMediaFilterAfterInterceptor.kt
@@ -0,0 +1,42 @@
+package com.angcyo.dsladapter.filter
+
+import com.angcyo.dsladapter.DslAdapterItem
+import com.angcyo.dsladapter.toNone
+
+/**
+ * 当数量不足时, 显示媒体添加item
+ * Email:angcyo@126.com
+ * @author angcyo
+ * @date 2020/03/25
+ * Copyright (c) 2019 ShenZhen O&M Cloud Co., Ltd. All rights reserved.
+ */
+open class AddMediaFilterAfterInterceptor : FilterAfterInterceptor {
+
+    /**显示的最大媒体数, 不足时. 追加[addMediaDslAdapterItem]*/
+    var maxMediaCount = Int.MAX_VALUE
+
+    var addMediaDslAdapterItem: DslAdapterItem? = null
+
+    /**激活[adapter]状态切换*/
+    var enableChangeAdapterState: Boolean = true
+
+    override fun intercept(chain: FilterAfterChain): List<DslAdapterItem> {
+        if (chain.requestList.size >= maxMediaCount) {
+            return chain.requestList.subList(0, maxMediaCount)
+        }
+
+        addMediaDslAdapterItem?.apply {
+
+            if (enableChangeAdapterState) {
+                chain.dslAdapter.toNone()
+            }
+
+            val result = mutableListOf<DslAdapterItem>()
+            result.addAll(chain.requestList)
+            result.add(this)
+            return result
+        }
+
+        return chain.requestList
+    }
+}
---
+++ Adapter/src/main/java/com/angcyo/dsladapter/filter/MaxItemCountFilterAfterInterceptor.kt
@@ -0,0 +1,22 @@
+package com.angcyo.dsladapter.filter
+
+import com.angcyo.dsladapter.DslAdapterItem
+
+/**
+ * 最大显示数量控制
+ * Email:angcyo@126.com
+ * @author angcyo
+ * @date 2020/03/25
+ * Copyright (c) 2019 ShenZhen O&M Cloud Co., Ltd. All rights reserved.
+ */
+open class MaxItemCountFilterAfterInterceptor : FilterAfterInterceptor {
+
+    var maxItemCount = Int.MAX_VALUE
+
+    override fun intercept(chain: FilterAfterChain): List<DslAdapterItem> {
+        if (chain.requestList.size >= maxItemCount) {
+            return chain.requestList.subList(0, maxItemCount)
+        }
+        return chain.requestList
+    }
+}
